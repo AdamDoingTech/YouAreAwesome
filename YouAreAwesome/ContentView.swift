67174621cd110e5c7dd2e89a8d8fcadcc6831398
AdamDoingTech <olmpy@hotmail.com>
2023-04-02 05:06:44 -0600

    create new costum colors in the Assets folder

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,42 +11,60 @@ struct ContentView: View {
     
     
     var body: some View {
-       VStack {
-           Spacer()
-           
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.orange)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
-                .padding()
-            
-           
-            Spacer()
-           
-           
-            HStack {
-                // First Button
-                Button("Awesome") {
-                    // This is action prefromend when the button is pressed.
-                    messageString = "You Are Awesome!"
+//            Rectangle()
+//                .fill(.black)
+//            Color("SkyBlue").opacity(0.8)
+//                .ignoresSafeArea()
+            VStack {
+               Text("something here ")
+                   .font(.largeTitle)
+                   .fontWeight(.black)
+               // black here is not a color, but it means the max thickness of the font, such as semi-bold for example, or bold.
+                   .foregroundColor(Color("Gold-BC"))
+                   .padding()
+                   .background(Color("Maroon-BC"))
+                   .cornerRadius(15)
+               
+               Spacer()
+               
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.orange)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 1)
+                    .padding()
+                
+               
+                Spacer()
+               
+               
+                HStack {
+                    // First Button
+                    Button("Awesome") {
+                        // This is action prefromend when the button is pressed.
+                        messageString = "You Are Awesome!"
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
                     
+                    Spacer()
+                    
+                    // Second Button
+                    Button("Great") {
+                        // This is action prefromend when the button is pressed.
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
-                // Second Button
-                Button("Great") {
-                    // This is action prefromend when the button is pressed.
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
+    //            .border(.pink, width: 5)
+                .padding()
+                
             }
-//            .border(.pink, width: 5)
-            .padding()
-        }
+        
     }
 }
 
