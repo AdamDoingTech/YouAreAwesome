5856c37da4553b54f76d501313f18039b2c08a97
AdamDoingTech <olmpy@hotmail.com>
2023-04-02 14:16:24 -0600

    remove not needed code lines

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,65 +6,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var messageString = "When the Genius Bar needs help, they call you, Adam!"
-    
+    @State private var messageString = ""
     
     var body: some View {
-//            Rectangle()
-//                .fill(.black)
-//            Color("SkyBlue").opacity(0.8)
-//                .ignoresSafeArea()
-            VStack {
-               Text("something here ")
-                   .font(.largeTitle)
-                   .fontWeight(.black)
-               // black here is not a color, but it means the max thickness of the font, such as semi-bold for example, or bold.
-                   .foregroundColor(Color("Gold-BC"))
-                   .padding()
-                   .background(Color("Maroon-BC"))
-                   .cornerRadius(15)
-               
-               Spacer()
-               
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.orange)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-    //                .border(.orange, width: 1)
-                    .padding()
-                
-               
-                Spacer()
-               
-               
-                HStack {
-                    // First Button
-                    Button("Awesome") {
-                        // This is action prefromend when the button is pressed.
-                        messageString = "You Are Awesome!"
-                        
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    // Second Button
-                    Button("Great") {
-                        // This is action prefromend when the button is pressed.
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-    //            .border(.pink, width: 5)
+        VStack {
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.orange)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
                 .padding()
+            
+            Spacer()
+            
+            HStack {
+                // First Button
+                Button("Awesome") {
+                    // This is action prefromend when the button is pressed.
+                    messageString = "You Are Awesome!"
+                    
+                }
+                .buttonStyle(.borderedProminent)
                 
+                Spacer()
+                
+                // Second Button
+                Button("Great") {
+                    // This is action prefromend when the button is pressed.
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
-        
+            .padding()
+            
+        }
     }
 }
 
