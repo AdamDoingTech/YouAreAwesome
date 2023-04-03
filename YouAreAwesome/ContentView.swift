ea3c87ba7d8daf42cd63546ad2c195a72aa7fe26
AdamDoingTech <olmpy@hotmail.com>
2023-04-02 18:19:34 -0600

    bring back default app state

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,44 +9,51 @@ struct ContentView: View {
     @State private var messageString = ""
     
     var body: some View {
-        VStack {
+        ZStack {
+            Rectangle()
+                .fill(.black)
+                .ignoresSafeArea()
             
-            Spacer()
-            
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.orange)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .padding()
-            
-            Spacer()
-            
-            HStack {
-                // First Button
-                Button("Awesome") {
-                    // This is action prefromend when the button is pressed.
-                    messageString = "You Are Awesome!"
+            VStack {
+                        
+                    Spacer()
+                        
+                    Text(messageString)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.orange)
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
+                        .padding()
                     
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Spacer()
-                
-                // Second Button
-                Button("Great") {
-                    // This is action prefromend when the button is pressed.
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    HStack {
+                        // First Button
+                        Button("Awesome") {
+                            // This is action prefromend when the button is pressed.
+                            messageString = "You Are Awesome!"
+                            
+                        }
+                        .buttonStyle(.borderedProminent)
+                        
+                        Spacer()
+                        
+                        // Second Button
+                        Button("Great") {
+                            // This is action prefromend when the button is pressed.
+                            messageString = "You Are Great!"
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    .padding()
+                    
             }
-            .padding()
-            
         }
-    }
+    } 
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
